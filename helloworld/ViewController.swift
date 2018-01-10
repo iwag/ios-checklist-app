5988d2093149ff3720233393ac661f792efe812f
岩上拓矢 <iwag@livedoor.com>
2018-01-09 18:42:17 -0800

    Initial Commit

---
+++ helloworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by 田中タカシ on 2018-01-09.
+//  Copyright © 2018 iwag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
